971ac47f6491f7bc17174235f5b62ab3f8cb0171
vacrain <726866011@qq.com>
2022-05-09 00:39:22 +0800

    test @fastify/mysql ok!

--- README.md
+++ README.md
@@ -2,18 +2,26 @@
  * @Author: vacrain
  * @Date: 2022-05-08 07:55:02
  * @LastEditors: vacrain
- * @LastEditTime: 2022-05-08 14:48:26
+ * @LastEditTime: 2022-05-09 00:37:05
  * @FilePath: /fastify-001/README.md
  * @Description:
  *
 -->
 
+使用：
+
+```
+pnpm install
+pnpm run serve
+```
+
+体验过程：
+
 ```
 npm init
 一路回车
 
-注意faker6不太好用，import不进来
-pnpm install fastify faker fastify-autoload fastify-plugin
+注意faker6不太好用，import不进来,要用5
 
 package.json里面添加
 "type":"module",
@@ -24,8 +32,19 @@ fastify.fastify-snippets
 http测试
 humao.rest-client
 
-cli:
+install cli:
 pnpm install fastify-cli -g
+
+use cli:
 fastify start -w -l info -P app.js
+
+create .env file push follow configs:
+SERVER_PORT = 3000
+DB_HOST = 127.0.0.1
+DB_USER = root
+DB_PASSWORD = 123456
+DB_DB1 = db01
+DB_PORT = 3306
+
 
 ```
--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: vacrain
  * @Date: 2022-05-08 07:58:25
  * @LastEditors: vacrain
- * @LastEditTime: 2022-05-08 10:30:21
+ * @LastEditTime: 2022-05-09 00:38:22
  * @FilePath: /fastify-001/app.js
  * @Description:
  *
@@ -11,6 +11,7 @@ import Fastify from "fastify";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import AutoLoad from "@fastify/autoload";
+import fastifyMySQL from "@fastify/mysql";
 
 import Route from "./test/route.js";
 
@@ -24,25 +25,67 @@ const __dirname = dirname(__filename);
 // console.log(__dirname);
 // /Users/wangxiansheng/Documents/projects/my_now/fastify-001
 
+const user = process.env.DB_USER,
+    host = process.env.DB_HOST,
+    password = process.env.DB_PASSWORD,
+    db1 = process.env.DB_DB1,
+    port = process.env.DB_PORT;
+
+const connectStr =
+    "mysql://" +
+    host +
+    ":" +
+    port +
+    "/" +
+    db1 +
+    "?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC&user=" +
+    user +
+    "&password=" +
+    password;
+
+// console.log(connectStr);
+
 export default async function (fastify, opts) {
     // 必须在autoload上面
     fastify.register(import("./plugins/index.js"));
+
+    fastify.register(fastifyMySQL, {
+        connectionString: connectStr,
+    });
 
     fastify.register(AutoLoad, {
         dir: join(__dirname, "routes"),
     });
 
-    // http://localhost:3456/test1
-    fastify.register(Route);
-    // 直接引入
-    // fastify.register(import("./test/route.js"));
+    fastify.get("/user/:id", (req, reply) => {
+        fastify.mysql.getConnection(onConnect);
 
-    // http://localhost:3456/
-    // fastify.get("/", async (req, reply) => {
-    //     return "hihihi";
-    // });
+        function onConnect(err, client) {
+            if (err) return reply.send(err);
 
-    // fastify.listen(3000, () => {
-    //     console.log("running on port 3000");
-    // });
+            client.query(
+                "SELECT * FROM people WHERE id=?",
+                [req.params.id],
+                function onResult(err, result) {
+                    client.release();
+                    reply.send(err || result);
+                }
+            );
+        }
+    });
 }
+
+// http://localhost:3456/test1
+// fastify.register(Route);
+
+// 直接引入
+// fastify.register(import("./test/route.js"));
+
+// http://localhost:3456/
+// fastify.get("/", async (req, reply) => {
+//     return "hihihi";
+// });
+
+// fastify.listen(3000, () => {
+//     console.log("running on port 3000");
+// });
